73a34bf5d2a7d49d1433248971f364e9509665c1
valjetaA <darjac6463@gmail.com>
2023-01-02 17:35:55 +0300

    добавила менюшку для фигур

--- src/components/toolBar/ToolBar.tsx
+++ src/components/toolBar/ToolBar.tsx
@@ -8,6 +8,9 @@ import addImage from "../../assets/add-image.svg";
 import addText from "../../assets/add-text.svg";
 import addFigure from "../../assets/add-figure.svg";
 import showSlides from "../../assets/showSlides.svg";
+import triangle from "../../assets/triangle.svg"
+import circle from "../../assets/circle.svg"
+import square from "../../assets/square.svg"
 import { addSlideHandler, removeSlideHandler } from "../editor/EditorFn";
 
 export function ToolBar(Props: { presentation: Presentation }) {
@@ -19,7 +22,13 @@ export function ToolBar(Props: { presentation: Presentation }) {
             <button className={styles.toolBarTool}><img src={forward} className={styles.icon}/></button>
             <button className={styles.toolBarTool}><img src={addImage} className={styles.icon}/></button>
             <button className={styles.toolBarTool}><img src={addText} className={styles.icon}/></button>
-            <button className={styles.toolBarTool}><img src={addFigure} className={styles.icon}/></button>
+            <button className={styles.toolBarTool + " " + styles.toolBarToolFigure}><img src={addFigure} className={styles.icon}/>
+              <div className={styles.toolBarToolMenu}>
+                <a href='#'><img src={triangle} className={styles.toolBarToolMenuFigure}/></a>
+                <a href='#'><img src={circle} className={styles.toolBarToolMenuFigure}/></a>
+                <a href='#'><img src={square} className={styles.toolBarToolMenuFigure}/></a>
+              </div>
+            </button>
             <button className={styles.toolBarTool}><img src={showSlides} className={styles.icon}/>Показ слайдов</button>
         </div>
     )
