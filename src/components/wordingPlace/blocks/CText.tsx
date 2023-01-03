b04b6fe5a8fcac88cc286c792f031c45ca43e043
tanyginvv <99418463+tanyginvv@users.noreply.github.com>
2023-01-03 20:57:58 +0300

    сделал так чтобы текст двигался, счастливая нимагу

--- src/components/wordingPlace/blocks/CText.tsx
+++ src/components/wordingPlace/blocks/CText.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import styles from './CText.module.css';
-
+import useDragger from '../../hooks/useDragger';
+let count = 0;
+let countstr = ""
 function CText(Props: {fontFamily: string, fontColor: string, fontSize: number, content: string}) {
-
+    count = count + 1;
+    countstr = count.toString();
+    useDragger(countstr);
     const textStyle = {
         fontFamily: Props.fontFamily,
         fontColor: Props.fontColor,
-        fontSize: Props.fontSize
+        fontsize: Props.fontSize
     }
 
     return (
-        <input className={styles.text} style={textStyle} type="textarea" defaultValue={Props.content} />
+        <input className={styles.text} style={textStyle} type="textarea" defaultValue={Props.content} id={countstr} />
     );
 };
 
