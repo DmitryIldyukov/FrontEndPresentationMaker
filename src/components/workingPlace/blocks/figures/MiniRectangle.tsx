0c0ee79182e67824e8925a2f5cad3d0558e4bf58
DmitryIldyukov <dima.ildyukov@mail.ru>
2023-01-11 12:01:20 +0300

    Доработал

--- src/components/workingPlace/blocks/figures/MiniRectangle.tsx
+++ src/components/workingPlace/blocks/figures/MiniRectangle.tsx
@@ -17,10 +17,10 @@ function MiniRectangle(Props: {
     }
 
     const MiniRectangleSizing = {
-        height: (Props.size.height / 4.4),
-        width: (Props.size.width / 4.37),
-        top: (Props.position.y / 4.4),
-        left: (Props.position.x / 4.37)
+        height: (Props.size.height / 5),
+        width: (Props.size.width / 5),
+        top: (Props.position.y / 5),
+        left: (Props.position.x / 5)
     }
 
     return (
